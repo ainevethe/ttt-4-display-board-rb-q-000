5d4f0895a0c291fd334dbabfaa87cf51248a08ac
Crista Cooper <aine3d@gmail.com>
2017-10-14 13:51:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 
 def display_board
 puts  "   |   |   "
@@ -10,5 +9,5 @@ puts  "-----------"
 puts  "   |   |   "
 end
 
-
+board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 display_board(board)
